d1a6aa645fde95d93d0c2632ac5a52f1a09bc6b7
Leira <arielberdah@outlook.com>
2019-11-03 18:23:20 +0200

    added new method to sort int array

--- src/com/datastruct/InsertSort.java
+++ src/com/datastruct/InsertSort.java
@@ -8,6 +8,15 @@ public class InsertSort {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		
+		/*
+		 * int arr[],key,prev;
+		next: for (int i = 1; i < arr.length; i++)
+					for (int j = 0; j < arr.length; j++)
+						if(prev > key)
+							arr[i]=arr[j];
+		*/
+		
+		
 		List<Integer> n = new LinkedList<Integer>();
 		int j[]= { 9,7,5,3,2,1 };
 
@@ -18,8 +27,31 @@ public class InsertSort {
 		}
 		
 		System.out.println(insertSort(n));
+		
+		int K[]={4,9,3,0,5,2,1};
+		for(int num:K){
+		System.out.print(num + " ");}
+		System.out.print("\ninsert sort int[] array:\n");
+		for(int num:insertSort(K)){
+		System.out.print(num + " ");}
 	}
 	
+	//===========================(int[])Array
+	public static int[] insertSort(int A[]){
+
+		for(int i=1; i<A.length;i++){
+			int tmp=0;
+				for(int j=i-1;j>=0;j--){
+					if(A[j+1]<A[j]){
+						tmp=A[j+1];
+						A[j+1]=A[j];
+						A[j]=tmp;}
+							}
+						}
+
+			return A; }
+	
+	//=====================================Collection
 	public static List<Integer> insertSort( List<Integer> numbers){
 		List<Integer> sortedList = new LinkedList<>();
 		int loop=0;
